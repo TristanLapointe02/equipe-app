37a36d4c8037b2b918fceb35ae92e5c40363cf18
Tristan Lapointe <tristan.lapointe02@gmail.com>
2021-01-27 15:10:58 -0500

    Utilisation de variable

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const nom = "React";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Yo</h2>
+
+        <h2>Allo {nom}</h2>
+
         <a
           className="App-link"
           href="https://reactjs.org"
