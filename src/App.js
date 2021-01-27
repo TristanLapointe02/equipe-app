45c82e20598bde5395434b06b9c265907fbf0df9
Tristan Lapointe <tristan.lapointe02@gmail.com>
2021-01-27 15:26:48 -0500

    blabla

--- src/App.js
+++ src/App.js
@@ -10,16 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <h2>Allo {nom}</h2>
+        <h2>Allo {nom.toUpperCase()} !</h2>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
